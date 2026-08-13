f6cd8067a8ab02cf0f8d48a64e89fd95e9944484
martiniclayton <68083141+martiniclayton@users.noreply.github.com>
2026-08-12 21:50:28 -0300

    adc filtro no portifolio

--- js/portifolio.js
+++ js/portifolio.js
@@ -1,18 +1,34 @@
 const obras = [
     {imagem: "img/obra01galpao.jpeg", nome: "Construção de Galpão", local: "Inajá, PE", categoria: "Construção"},
-    {imagem: "img/modern-city-construction-site.jpg", nome: "Residência Horizonte", local: "Residencial · Alphaville, SP", categoria: "Construção"},
-    {imagem: "img/heavy-excavator-digging-day-light.jpg", nome: "Residência Horizonte", local: "Residencial · Alphaville, SP", categoria: "Construção"},
-    {imagem: "img/imgCardBradesco.png", nome: "Residência Horizonte", local: "Residencial · Alphaville, SP", categoria: "Construção"},
+    {imagem: "img/portifolio/ReformaBancoBradesco/imgCardBradesco.png", nome: "Reforma Banco Bradesco", local: " Buique, PE", categoria: "Reforma"},
+    {imagem: "img/portifolio/obraCasasInajá/WhatsApp\ Image\ 2026-08-12\ at\ 20.19.36.jpeg", nome: "Residências Sociais", local: "Inajá, PE", categoria: "Construção"},
+    {imagem: "img/imgCardBradesco.png", nome: "Residência Horizonte", local: "Residencial · Alphaville, SP", categoria: "Construção"}, 
     {imagem: "img/low-angle-perspective-tree-with-beautiful-canopy.jpg", nome: "Residência Horizonte", local: "Residencial · Alphaville, SP", categoria: "Reforma"},
 ]
 
 function filtrar(categoria){
+
+    if(categoria === "Todos"){
+        atualizarTela(obras);
+        return;
+    }
     const filtrado = obras.filter(cat => cat.categoria === categoria);
 
     atualizarTela(filtrado);
     return filtrado;
 }
 
+function filtragem(botao){
+
+    const btns = document.querySelectorAll(".btn-fiiltro")
+
+    btns.forEach(btn =>{
+        btn.classList.remove("clicado")
+    })
+    botao.classList.add("clicado");
+
+    filtrar(botao.value);
+}
 
 
 
@@ -21,6 +37,12 @@ const containerPortifolio = document.getElementById("container-obras");
 
 function atualizarTela(arrayObras){
     containerPortifolio.innerHTML = ``;
+
+    if(arrayObras.length === 0){
+        containerPortifolio.innerHTML = `
+            <h2> Não possui obras dessa categoria.</h2>
+        `
+    }
 
     arrayObras.forEach((obra, index) =>{
 
